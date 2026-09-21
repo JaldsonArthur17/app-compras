1ac13f835102ac133cb076e20b0817102f4735df
jaldsonarthur17 <jaldsonarthur1322@gmail.com>
2026-09-21 20:53:53 -0300

    exibição de produtos no limit agora é 50

--- hooks/useHome.js
+++ hooks/useHome.js
@@ -41,7 +41,7 @@ export default function useHome() {
   const [activeSort, setActiveSort] = useState('recent');
 
   useEffect(() => {
-    axios.get('https://dummyjson.com/products?limit=10')
+    axios.get('https://dummyjson.com/products?limit=50')
       .then(({ data }) => {
         setProducts(data.products);
         const cats = ['all', ...new Set(data.products.map(p => p.category))];
